1c84a95fde80ee4b406ac237eb3ce2674c5302e1
Bogdan Skrypnyuk <shtang3r@gmail.com>
2018-11-29 22:08:31 +0200

    Implemented Resister and Login functionality

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,8 @@ namespace DatingApp.API.Controllers
         {
              _dbContext = dbContext;
         }
-        // GET api/values
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
