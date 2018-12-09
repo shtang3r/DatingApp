3420765009cebc870e529c54d080d3927cede5df
Bogdan Skrypnyuk <shtang3r@gmail.com>
2018-12-09 17:38:34 +0200

    removed values and attempted to organize local mysql migrations

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,7 +7,6 @@ namespace DatingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) :base(options) { }
 
-        public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
